02f39209437fd251fa3e4293831a5a4599c30d50
Yiu Chung Yau <yiuchungyau@gmail.com>
2018-11-07 17:30:23 -0500

    update Edge

--- Graph/Edge.java
+++ Graph/Edge.java
@@ -1,13 +1,15 @@
 public class Edge{
 
 	String name;
+	int weight;
 	Vertex from;
 	Vertex to;
 
-	public Edge(Vertex from, Vertex to, String name){
+	public Edge(Vertex from, Vertex to, String name, int weight){
 		this.from = from;
 		this.to = to;
 		this.name = name;
+		this.weight = weight;
 	}
 
 }
